685e62dd0dc5b948f4d81f492639e4218f5aed0c
Gerasimos Michalitsianos <gerasimosmichalitsianos@gmail.com>
2022-03-20 22:05:18 -0400

    Delete pansharpen.h

--- pansharpen.h
+++
@@ -1,6 +0,0 @@
-#ifndef PANSHARPEN_H_
-#define PANSHARPEN_H_
-void pansharpen(Geotiff,Geotiff,Geotiff,Geotiff,Geotiff);
-void pansharpenFIHS(Geotiff,Geotiff,Geotiff,Geotiff,Geotiff);
-void pansharpenBrovey(Geotiff,Geotiff,Geotiff,Geotiff,Geotiff);
-#endif
